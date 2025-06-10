17259e3ae7b451b7fceb774f06084e52a323b6d8
RealTribn <tristan.vandermeer@gmail.com>
2025-06-11 00:05:54 +0100

    learning syntax

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 // Today we are learning
 #include <iostream>
+#pragma once // <---- Still learning about this
+
 using namespace std;
 
 int main() {
     cout << "Ok so new language is hard";
     return 0;
 }
+
+int square(const int x) { // Just need to learn the syntax
+    return x*x;
+}
+
+class MyClass { // Class definition
+public:
+    int val;
+};
